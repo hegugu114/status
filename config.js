dd53c8804fe9fd33d126ee682cdc67cc9b877561
hegugu114 <120194840+hegugu114@users.noreply.github.com>
2023-01-23 22:41:20 +0800

    Edit config

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 window.Config = {
 
   // 显示标题
-  SiteName: 'Public Status',
+  SiteName: 'Heyc\'s Site Status',
 
   // UptimeRobot Api Keys
   // 支持 Monitor-Specific 和 Read-Only
   ApiKeys: [
-    'm784488775-dd1ad84b209c05f8e185c33e',
-    'm784490063-7b5da437e7f1e0d67613714d',
-    'm784497419-de55aa09902ccb3ab22d548a',
-    'm784496436-71a4bf7b1e3bdf7756be131b',
+    'ur1988426-37719d19ed7ff934eea9d4b3',
   ],
 
   // 日志天数
-  CountDays: 90,
+  CountDays: 60,
 
   // 是否显示检测站点的链接
   ShowLink: true,
@@ -21,16 +18,12 @@ window.Config = {
   // 导航栏菜单
   Navi: [
     {
-      text: 'Homepage',
-      url: 'https://status.org.cn/'
+      text: 'Stable',
+      url: 'https://stable.heyc.eu.org/'
     },
     {
-      text: 'GitHub',
-      url: 'https://github.com/yb/uptime-status'
-    },
-    {
-      text: 'Blog',
-      url: 'https://abo.xyz/'
+      text: 'GH Pages',
+      url: 'https://ghpages.heyc.eu.org'
     },
   ],
 };
